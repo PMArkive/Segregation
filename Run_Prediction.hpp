0c281db25609752b8d16aa76bceff880cb340493
explorer <explorer@explorer.explorer>
2022-05-18 23:14:05 +0300

    nothing interesting (style mismatch)

--- Run_Prediction.hpp
+++ Run_Prediction.hpp
@@ -4,9 +4,9 @@ void Redirected_Run_Prediction()
 {
 	if (*(__int8*)540609192 == 6)
 	{
-		static void* Prediction = *(void**)540494880;
-
 		using Update_Type = void(__thiscall*)(void* Prediction, void* Unknown_Parameter_1, __int8 Unknown_Parameter_2, void* Unknown_Parameter_3, __int32 Unknown_Parameter_4);
+
+		static void* Prediction = *(void**)540494880;
 
 		Update_Type(605209536)(Prediction, *(void**)540609292, 1, *(void**)540627876, *(__int32*)540627868 + *(__int32*)540627872);
 	}
