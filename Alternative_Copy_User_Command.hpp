1dc6e9fc281d49fa37e533e66701bab7504067ea
explorer <explorer@explorer.explorer>
2022-07-11 17:23:15 +0300

    Commit

--- Alternative_Copy_User_Command.hpp
+++ Alternative_Copy_User_Command.hpp
@@ -293,7 +293,7 @@ void __thiscall Redirected_Copy_User_Command(void* Unknown_Parameter, User_Comma
 
 								Target_Structure Target =
 								{
-									Player_Data->Priority,
+									Player_Data->Priority == -2 ? (__int8)0 : Player_Data->Priority,
 
 									Square_Root(__builtin_powf(Local_Player_Origin[0] - Entity_Origin[0], 2) + __builtin_powf(Local_Player_Origin[1] - Entity_Origin[1], 2) + __builtin_powf(Local_Player_Origin[2] - Entity_Origin[2], 2)),
 
