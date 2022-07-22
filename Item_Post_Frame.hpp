b19e528d205b9c9cad0622f017b70a9a43c9d034
explorer <explorer@explorer.explorer>
2022-07-22 17:25:59 +0300

    forgot

--- Item_Post_Frame.hpp
+++ Item_Post_Frame.hpp
@@ -1,18 +1,27 @@
 #pragma once
 
-void* Original_Item_Post_Frame_Caller_Location;
+void* Original_Player_Item_Post_Frame_Caller_Location;
 
-void __thiscall Redirected_Item_Post_Frame(void* Weapon)
+void* Original_Weapon_Item_Post_Frame_Caller_Location;
+
+void __thiscall Redirected_Item_Post_Frame(void* Reserved)
 {
 	if (__builtin_signbitf(Shot_Time) == 0)
 	{
-		float Accuracy = *(float*)((unsigned __int32)Weapon + 1888);
-
-		(decltype(&Redirected_Item_Post_Frame)(Original_Item_Post_Frame_Caller_Location))(Weapon);
-
-		if (Is_First_Command_Predicted == 1)
+		if (__builtin_return_address(0) == (void*)604316089)
 		{
-			*(float*)((unsigned __int32)Weapon + 1888) = Accuracy;
+			(decltype(&Redirected_Item_Post_Frame)(Original_Player_Item_Post_Frame_Caller_Location))(Reserved);
+		}
+		else
+		{
+			float Accuracy = *(float*)((unsigned __int32)Reserved + 1888);
+
+			(decltype(&Redirected_Item_Post_Frame)(Original_Weapon_Item_Post_Frame_Caller_Location))(Reserved);
+
+			if (Is_First_Command_Predicted == 1)
+			{
+				*(float*)((unsigned __int32)Reserved + 1888) = Accuracy;
+			}
 		}
 	}
 }
