60468502d5345a84fcff314bb7abf829e7d3ba9b
explorer <explorer@explorer.explorer>
2022-08-07 10:45:51 +0300

    nothing

--- Extended_Interface.hpp
+++ Extended_Interface.hpp
@@ -55,16 +55,16 @@ Interface_Structure Interface_Uber_Alles_Speed;
 
 void Implement_Extended_Interface()
 {
-	auto Create_Interface = [](Interface_Structure* Variable, char* Name, char* Value, void* Handler)
+	auto Create_Console_Variable = [](Interface_Structure* Variable, char* Name, char* Value, void* Handler)
 	{
-		using Create_Handled_Interface_Type = void(__thiscall*)(Interface_Structure* Variable, char* Name, char* Value, void* Unknown_Parameter_1, void* Unknown_Parameter_2, void* Handler);
+		using Create_Console_Variable_Type = void(__thiscall*)(Interface_Structure* Variable, char* Name, char* Value, void* Unknown_Parameter_1, void* Unknown_Parameter_2, void* Handler);
 
-		Create_Handled_Interface_Type(539131040)(Variable, Name, Value, nullptr, nullptr, Handler);
+		Create_Console_Variable_Type(539131040)(Variable, Name, Value, nullptr, nullptr, Handler);
 
 		wprintf(L"[ + ] %hs -> %p\n", Name, Variable);
 	};
 
-	Create_Interface(&Interface_Commentator, (char*)"Commentator", (char*)"1", nullptr);
+	Create_Console_Variable(&Interface_Commentator, (char*)"Commentator", (char*)"1", nullptr);
 
 	using Create_Console_Command_Type = void(__thiscall*)(Interface_Structure* Variable, char* Name, void* Handler, void* Unknown_Parameter_1, void* Unknown_Parameter_2, void* Unknown_Parameter_3);
 
@@ -72,37 +72,37 @@ void Implement_Extended_Interface()
 
 	wprintf(L"[ + ] Get_Priorities %p\n", &Interface_Get_Priorities);
 
-	Create_Interface(&Interface_Set_Priority, (char*)"Set_Priority", (char*)"1 0", (void*)Set_Priority);
+	Create_Console_Variable(&Interface_Set_Priority, (char*)"Set_Priority", (char*)"1 0", (void*)Set_Priority);
 
-	Create_Interface(&Interface_Bruteforce_Memory, (char*)"Bruteforce_Memory", (char*)"1", (void*)Bruteforce_Reset_Memory);
+	Create_Console_Variable(&Interface_Bruteforce_Memory, (char*)"Bruteforce_Memory", (char*)"1", (void*)Bruteforce_Reset_Memory);
 
-	Create_Interface(&Interface_Bruteforce_Tolerance, (char*)"Bruteforce_Tolerance", (char*)"0", (void*)Bruteforce_Reset_Tolerance);
+	Create_Console_Variable(&Interface_Bruteforce_Tolerance, (char*)"Bruteforce_Tolerance", (char*)"0", (void*)Bruteforce_Reset_Tolerance);
 
-	Create_Interface(&Interface_Bruteforce, (char*)"Bruteforce", (char*)"1", (void*)Bruteforce_Reset);
+	Create_Console_Variable(&Interface_Bruteforce, (char*)"Bruteforce", (char*)"1", (void*)Bruteforce_Reset);
 
-	Create_Interface(&Interface_Bruteforce_Angles, (char*)"Bruteforce_Angles", (char*)"0, -45, 45, -90, 90", (void*)Bruteforce_Set_Angles);
+	Create_Console_Variable(&Interface_Bruteforce_Angles, (char*)"Bruteforce_Angles", (char*)"0, -45, 45, -90, 90", (void*)Bruteforce_Set_Angles);
 
 	Bruteforce_Set_Angles(&Interface_Bruteforce_Angles);
 
-	Create_Interface(&Interface_Alternative, (char*)"Alternative", (char*)"0", nullptr);
+	Create_Console_Variable(&Interface_Alternative, (char*)"Alternative", (char*)"0", nullptr);
 
-	Create_Interface(&Interface_Minimum_Choked_Commands, (char*)"Minimum_Choked_Commands", (char*)"2", nullptr);
+	Create_Console_Variable(&Interface_Minimum_Choked_Commands, (char*)"Minimum_Choked_Commands", (char*)"2", nullptr);
 
-	Create_Interface(&Interface_Maximum_Choked_Commands, (char*)"Maximum_Choked_Commands", (char*)"21", nullptr);
+	Create_Console_Variable(&Interface_Maximum_Choked_Commands, (char*)"Maximum_Choked_Commands", (char*)"21", nullptr);
 
-	Create_Interface(&Interface_Aim_Intersection, (char*)"Aim_Intersection", (char*)"1", nullptr);
+	Create_Console_Variable(&Interface_Aim_Intersection, (char*)"Aim_Intersection", (char*)"1", nullptr);
 
-	Create_Interface(&Interface_Aim_Height, (char*)"Aim_Height", (char*)"0.9", nullptr);
+	Create_Console_Variable(&Interface_Aim_Height, (char*)"Aim_Height", (char*)"0.9", nullptr);
 
-	Create_Interface(&Interface_Angle_X, (char*)"Angle_X", (char*)"180", nullptr);
+	Create_Console_Variable(&Interface_Angle_X, (char*)"Angle_X", (char*)"180", nullptr);
 
-	Create_Interface(&Interface_First_Choked_Angle_Y, (char*)"First_Choked_Angle_Y", (char*)"46", nullptr);
+	Create_Console_Variable(&Interface_First_Choked_Angle_Y, (char*)"First_Choked_Angle_Y", (char*)"46", nullptr);
 
-	Create_Interface(&Interface_Second_Choked_Angle_Y, (char*)"Second_Choked_Angle_Y", (char*)"136", nullptr);
+	Create_Console_Variable(&Interface_Second_Choked_Angle_Y, (char*)"Second_Choked_Angle_Y", (char*)"136", nullptr);
 
-	Create_Interface(&Interface_Angle_Y, (char*)"Angle_Y", (char*)"-135", nullptr);
+	Create_Console_Variable(&Interface_Angle_Y, (char*)"Angle_Y", (char*)"-135", nullptr);
 
-	Create_Interface(&Interface_Uber_Alles_Scale, (char*)"Uber_Alles_Scale", (char*)"16", nullptr);
+	Create_Console_Variable(&Interface_Uber_Alles_Scale, (char*)"Uber_Alles_Scale", (char*)"16", nullptr);
 
-	Create_Interface(&Interface_Uber_Alles_Speed, (char*)"Uber_Alles_Speed", (char*)"192", nullptr);
+	Create_Console_Variable(&Interface_Uber_Alles_Speed, (char*)"Uber_Alles_Speed", (char*)"192", nullptr);
 }
