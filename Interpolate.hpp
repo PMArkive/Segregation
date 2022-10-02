a656afb76ffe906bdf9c102d1412dd7c9295c158
explorer <explorer@explorer.explorer>
2022-10-02 14:53:40 +0300

    nothing

--- Interpolate.hpp
+++ Interpolate.hpp
@@ -1,20 +1,14 @@
 void* Original_Interpolate_Caller_Location;
 
-__int8 __thiscall Redirected_Interpolate(void* Player, void* Unknown_Parameter)
+__int8 __thiscall Redirected_Interpolate(void* Entity, void* Unknown_Parameter)
 {
-	void* Local_Player = *(void**)607867332;
-
-	if (Player != Local_Player)
+	if (Entity != *(void**)607867332)
 	{
-		*(__int8*)607715194 = 0;
+		if (*(void**)Entity == (void*)607350148)
+		{
+			return 1;
+		}
 	}
 
-	__int8 Return_Value = (decltype(&Redirected_Interpolate)(Original_Interpolate_Caller_Location))(Player, Unknown_Parameter);
-
-	if (Player != Local_Player)
-	{
-		*(__int8*)607715194 = 1;
-	}
-
-	return Return_Value;
+	return (decltype(&Redirected_Interpolate)(Original_Interpolate_Caller_Location))(Entity, Unknown_Parameter);
 }
